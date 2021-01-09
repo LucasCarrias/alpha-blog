dca62e6f94e1fbe2ffedcdc121adcbb2eaef9b57
Lucas Carrias <lucascarrias@outlook.com>
2021-01-08 23:29:34 -0300

    Create category tests, migration and model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_08_001529) do
+ActiveRecord::Schema.define(version: 2021_01_08_234956) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -19,6 +19,12 @@ ActiveRecord::Schema.define(version: 2021_01_08_001529) do
     t.datetime "updated_at", precision: 6, null: false
     t.boolean "active"
     t.integer "user_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
